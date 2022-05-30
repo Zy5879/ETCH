7d8912cd7f6114d637bef74405fba4af5a5a62f2
Zy5879 <ZaireM21@gmail.com>
2022-05-30 13:53:42 -0400

    changed how my grid displays on page

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const container = document.getElementById("container");
-
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (i = 0; i < (rows * cols); i++) {
-    const square = document.createElement("div");
-    container.appendChild(square).className = "grid-item";
+function makeRows(size) {
+    let box = document.querySelector(".box");
+    box.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    box.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  //container.style.setProperty('--grid-rows', rows);
+ // container.style.setProperty('--grid-cols', cols);
+  for (i = 0; i < 257; i++) {
+    let square = document.createElement("div");
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = `${randomColor()}`
+    });
+    //square.style.backgroundColor = `${randomColor()}`;
+    box.insertAdjacentElement('beforeend',square);
   };
-  hoverCells();
+ //hoverCells();
 };
 
 function randomColor() {
@@ -19,13 +24,18 @@ function randomColor() {
     return colors;
 }
 
-function hoverCells() {
-    let items = document.querySelectorAll('.grid-item')
+/*function hoverCells() {
+    let items = document.querySelectorAll('.box')
     items.forEach(item => {
         item.addEventListener('mouseover', () => {
             item.style.backgroundColor = `${randomColor()}`;
         });
     });
 }
+*/
 
-makeRows(16,16);
+makeRows(16);
+
+function newSize(input) {
+    makeRows(input);
+}
